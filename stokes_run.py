a7c37a79a023ae5d901d9a1c78153e41fb5dd4b3
Sarah Dennis <sarahdennis@brandeis.edu>
2026-02-14 13:19:35 -0500

    Update stokes_run.py

--- stokes_run.py
+++ stokes_run.py
@@ -23,52 +23,20 @@ Re=0
 
 
 #------------------------------------------------------------------------------
-# h_in = 2
-# h_out = 1
-# l_in = 8
-# l_out=8
-# args = [h_in, h_out, l_in, l_out]
-# Example = examples.BFS
-
-
-#------------------------------------------------------------------------------
 h_in = 2
 h_out = 1
 l_in = 8
 l_out=8
-
-# xr = 0.35
-# yr = 0.4
-# xr_0p75 = 0.2625
-# yr_0p75 = 0.3
-xr_0p5 = 0.175
-yr_0p5 = 0.2
-xr_0p25 = 0.0875
-yr_0p25 = 0.1
-
-# args = [h_in, h_out, l_in, l_out, xr, yr]
-# Example = examples.BFS_wedge
-
-# args = [h_in, h_out, l_in, l_out, xr_0p5, yr_0p5]
-# Example = examples.BFS_wedge
-
-# args = [h_in, h_out, l_in, l_out, xr_0p75, yr_0p75]
-# Example = examples.BFS_wedge
-args = [h_in, h_out, l_in, l_out, xr_0p25, yr_0p25]
-Example = examples.BFS_wedge
+args = [h_in, h_out, l_in, l_out]
+Example = examples.BFS
 
 #------------------------------------------------------------------------------
-
 # H=2
 # h=1
 # L=16
-
-
 # delta=1/2
-
 # args = [H, h, L, delta]
 # Example = examples.BFS_pwl
-
 
 #------------------------------------------------------------------------------
 # H=2
@@ -78,7 +46,6 @@ Example = examples.BFS_wedge
 # args = [H, h, L, delta]
 # Example = examples.Logistic
 
-
 #------------------------------------------------------------------------------
 # H = 2
 # delta = 1/4
@@ -87,21 +54,6 @@ Example = examples.BFS_wedge
 # L = 8
 # args = [H, delta, k,  l, L]
 # Example = examples.Sinusoid
-
-
-#------------------------------------------------------------------------------
-# H = 3
-# L = 2 
-# args = [H, L]# tri slope = 2H/L
-# Example = examples.TriCavity
-
-#------------------------------------------------------------------------------
-# H = 4
-# xr = 0.75
-# L = 2 
-# args = [H, xr, L]# tri slope = 2H/L
-# Example = examples.TrapCavity
-
 
 #------------------------------------------------------------------------------
 # l=7
@@ -133,9 +85,6 @@ solver.load_run(N)
 
 # ------------------------------------------------------------------------------
 # solver.compare(args, U, Q, Re, 10,[20,40,80,160],320)
-# solver.compare(args, U, Q, Re, 20,[40,80],160)
-# solver.compare(args, U, Q, Re, 20,[40,80,160],320)
-# solver.compare(args, U, Q, Re, 10,[20,40,80],160)
 
 
 
